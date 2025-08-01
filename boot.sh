632a8a841224d0a44e639846e046be41a615370b
Fdo <rivas.aguilera@gmail.com>
2025-07-31 23:59:50 -0400

    test03: zsh's fucking w/me

--- boot.sh
+++ boot.sh
@@ -11,13 +11,13 @@ ansi_art='
                                                    ███    ███   ▀                   '
 
 clear
-print -e "\n$ansi_art\n"
+echo -e "\n$ansi_art\n"
 
 # sudo pacman -Sy --noconfirm --needed git
 
-print -e "\nCloning Autarky..."
+echo -e "\nCloning Autarky..."
 rm -rf ~/.local/share/autarky/
 git clone https://github.com/frivasa/autarky.git ~/.local/share/autarky >/dev/null
 
-print -e "\nInstallation starting..."
+echo -e "\nInstallation starting..."
 source ~/.local/share/autarky/install.sh
