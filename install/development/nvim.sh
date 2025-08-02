67854f34876e9a8172ecb733993be164b46ce509
Fdo <rivas.aguilera@gmail.com>
2025-08-02 13:49:28 -0400

    pre-pywal. fixes in install.

--- install/development/nvim.sh
+++ install/development/nvim.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
 
 if ! command -v nvim &>/dev/null; then
-  yay -S --noconfirm --needed neovim luarocks tree-sitter-cli
+  yay -S --noconfirm --needed neovim tree-sitter-cli
 
   # Install own vim
-  rm -rf ~/.config/nvim
-  cp -R ~/.local/share/autarky/config/nvim/* ~/.config/nvim/
-  # echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
+  cp -rf ~/.local/share/autarky/config/nvim/* ~/.config/nvim/
 fi
