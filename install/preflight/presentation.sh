50ada504b4bdd9ca17675d6af69ca98d246a539f
Fernando Rivas <rivas.aguilera@gmail.com>
2025-08-03 14:58:22 -0400

    nvim soft-landing edits [updates to obsidian.nvim too], python-gobject added jic early in the install proc.

--- install/preflight/presentation.sh
+++ install/preflight/presentation.sh
@@ -1,3 +1,6 @@
 #!/bin/zsh
 
-yay -S --noconfirm --needed gum python-terminaltexteffects
+yay -S --noconfirm --needed gum python-terminaltexteffects python-gobject
+
+# python-gobject installed further down the line but it is missing at 
+# somepoint in between step 1 and 3.
