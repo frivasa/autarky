265d4648b37177324b272cd06d7476c7930ab03e
Fernando Rivas <rivas.aguilera@gmail.com>
2025-10-01 05:59:11 -0300

    removed zoxide, back to full top waybar, remove omz ancillary files
    useless with fzf and typically quantum leaps to wherever on typos or
    similar named folders.
    back to full top waybar bc waybar cannot selectively allow click-through
    so transparent bits are dead-zones.

--- install/development/terminal.sh
+++ install/development/terminal.sh
@@ -2,6 +2,6 @@
 
 yay -S --noconfirm --needed \
   wget curl unzip inetutils impala \
-  fd eza fzf ripgrep zoxide bat jq \
+  fd eza fzf ripgrep bat jq \
   wl-clipboard fastfetch btop \
   man tldr less whois plocate
