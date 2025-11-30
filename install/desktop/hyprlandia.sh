68fc2622fe7dd1f622f67762d7b920a4479cfbc1
Fernando Rivas <rivas.aguilera@gmail.com>
2025-11-29 22:28:44 -0300

    walker/elephant fix.

--- install/desktop/hyprlandia.sh
+++ install/desktop/hyprlandia.sh
@@ -2,5 +2,7 @@
 
 yay -S --noconfirm --needed \
   hyprland hyprshot hyprpicker hyprlock hypridle polkit-gnome hyprland-qtutils \
-  walker elephant elephant-desktopapplications libqalculate waybar mako swaybg swayosd \
+  walker elephant-bin elephant-desktopapplications-bin elephant-symbols-bin elephant-websearch-bin \
+  elephant-calc-bin elephant-providerlist-bin \
+  libqalculate waybar mako swaybg swayosd \
   xdg-desktop-portal-hyprland xdg-desktop-portal-gtk
