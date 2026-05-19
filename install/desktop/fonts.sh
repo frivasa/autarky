ae0ba601f3d6a9cbc4b0a4b635d23f053917054f
Fernando Rivas <rivas.aguilera@gmail.com>
2026-05-19 17:33:18 +1200

    font switch (jetbrainsmono to Iosevka). rm redundand hyprland.confs. rm big ass latex package. super+w close signal instead of kill

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -3,5 +3,7 @@
 yay -S --noconfirm --needed ttf-font-awesome ttf-ia-writer noto-fonts noto-fonts-emoji
 
 if [ -z "$AUTARKY_BARE" ]; then
-  yay -S --noconfirm --needed ttf-jetbrains-mono noto-fonts-cjk noto-fonts-extra
+  # relic here to remember the older font: jetbrains-mono-nerd
+  # yay -S --noconfirm --needed ttf-jetbrains-mono-nerd noto-fonts-cjk noto-fonts-extra
+  yay -S --noconfirm --needed ttf-iosevka-nerd noto-fonts-cjk noto-fonts-extra
 fi
--- install/development/latex.sh
+++ install/development/latex.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# install everything, it's about 600 MB, might be skimmable to 350 but w/e
+# install everything, it's about 600 MB (texlive-fontsextra is 2 GB. Avoid at all costs)
 sudo pacman -S --noconfirm --needed \
   python-pylatexenc texlive-basic texlive-latex texlive-latexextra \
-  texlive-latexrecommended texlive-fontsextra \
-  texlive-fontsrecommended texlive-mathscience texlive-xetex
+  texlive-latexrecommended texlive-fontsrecommended texlive-mathscience texlive-xetex
 
