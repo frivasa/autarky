4db07514431c989dac7c6139881f982de2440a7c
Fernando Rivas <rivas.aguilera@gmail.com>
2026-06-24 18:41:05 +1200

    mpv, nnn, scaling
    mpv integration with nnn with umpv script (queues videos to mpv)
    nnn now configs via a binary wrapper instead of environment.d/nnn.conf
    (easier config and applies to any method for launching nnn)
    resetting some scaling numbers to handle qt apps better or at least not
    as wonky.

--- config/hypr/monitors.lua
+++ config/hypr/monitors.lua
@@ -1,11 +1,11 @@
 -- # Laptop panel (master)
-hl.env("GDK_SCALE", 2)
+-- hl.env("GDK_SCALE", 2)
 
 hl.monitor({
 	output = "eDP-1",
 	mode = "3200x1800@60",
 	position = "0x0",
-	scale = 2,
+	scale = 1.67,
 })
 
 hl.monitor({
--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -14,3 +14,13 @@ yay -S --noconfirm --needed \
   mpv imv wf-recorder \
   zen-browser-bin chromium video-downloader
 
+# enable mpv-nnn playlist interaction (; p)
+sudo cp /usr/share/mpv/scripts/umpv ~/.local/bin/umpv
+sudo chmod +x ~/.local/bin/umpv
+cp --parents ~/.local/share/autarky/config/nnn/plugins/addtoplaylist ~/.config/nnn/plugins/addtoplaylist
+sudo chmod +x ~/.config/nnn/plugins/addtoplaylist
+# this enables sending selected files to mpv playlist from nnn
+
+# use a nnn wrapper for easier configuration across launch methods (shell or fuzzel)
+sudo cp ~/.local/share/autarky/bin/nnn ~/.local/bin/nnn
+sudo chmod +x ~/.local/bin/nnn
