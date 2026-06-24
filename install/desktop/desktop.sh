88a5b0df195526eb3ca54ffee63639a6a3184b7d
Fernando Rivas <rivas.aguilera@gmail.com>
2026-06-24 21:00:38 +1200

    fcitx5 removal. waybar adjustments.

--- install/desktop/desktop.sh
+++ install/desktop/desktop.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo pacman -S --noconfirm --needed keepassxc syncthing \
   nodejs npm tree-sitter-cli unzip qbittorrent ollama \
-  python-json5 python-pywal fcitx5-configtool \
+  python-json5 python-pywal \
   hplip baobab android-file-transfer \
   gnome-disk-utility nautilus nnn \
   nwg-look inkscape gnumeric zathura zathura-pdf-mupdf evolution fuzzel
 
 yay -S --noconfirm --needed \
   brightnessctl playerctl pamixer wiremix wireplumber \
-  fcitx5 fcitx5-gtk fcitx5-qt wl-clip-persist \
+  wl-clip-persist \
   ffmpegthumbnailer python-pywalfox \
   slurp satty \
   mpv imv wf-recorder \
